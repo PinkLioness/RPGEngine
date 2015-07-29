ee73c25d73ee5f1db4881c6a99d6b8aff61980c2
PinkLioness <joao_albertoni+github@hotmail.com>
2015-07-29 20:35:01 -0300

    added different types of meters

--- interface.js
+++ interface.js
@@ -12,7 +12,9 @@ GAME.interface = {
 	statContainers:{ // These variables are merely suggestions, they should be the same as the player stats all the time but might not be
 		hp:undefined,
 		lust:undefined,
-		bellyVolume:undefined,
+		money:undefined,
+		assVolume:undefined,
+		vaginalVolume:undefined,
 		////////////////////////////
 		strength:undefined,
 		dexterity:undefined,
@@ -60,9 +62,7 @@ GAME.interface = {
 		// TODO: updating
 	},
 
-	initMeter:function(name, max){
-		var meter = new GAME.interface.classes.statBar(name, max);
-
+	addMeter:function(meter){
 		GAME.interface.statArea.appendChild(meter.container);
 
 		return meter;
@@ -87,71 +87,30 @@ GAME.interface = {
 		GAME.interface.setWindowSize();
 		window.addEventListener('resize', GAME.interface.setWindowSize);
 
+
+
 		
-		GAME.interface.statContainers['hp'] = GAME.interface.initMeter("HP");
-		GAME.interface.statContainers['lust'] = GAME.interface.initMeter("Lust");
-		GAME.interface.statContainers['bellyVolume'] = GAME.interface.initMeter("Belly volume");
+		GAME.interface.statContainers['hp'] = GAME.interface.addMeter(new GAME.interface.classes.statBar('HP'));
+		GAME.interface.statContainers['lust'] = GAME.interface.addMeter(new GAME.interface.classes.statBar("Lust"));
+		GAME.interface.statContainers['money'] = GAME.interface.addMeter(new GAME.interface.classes.meterlessStatBar("Money", "copper", true));
+
+		var divisions = [
+			{message:'Empty', percentage:0},
+			{message:'A bit of', percentage:1},
+			{message:'Pretty full with', percentage:25},
+			{message:'Bloated with', percentage:50},
+			{message:'Bursting with', percentage:70},
+			{message:'Overflowing with', percentage:80},
+			{message:'Bursting with', percentage:90}
+		];
+		GAME.interface.statContainers['assVolume'] = GAME.interface.addMeter(new GAME.interface.classes.textStatBarWithTypeOfContents('Ass', divisions, 100));
+		GAME.interface.statContainers['vaginalVolume'] = GAME.interface.addMeter(new GAME.interface.classes.textStatBarWithTypeOfContents('Vaginal', divisions, 100));
+
 		GAME.interface.statArea.appendChild(document.createElement('hr'));
 		
 		for(var stat in GAME.p.stats){ // Using the player stats to keep things updated, the interface class variables are merely suggestions
 			var capitalizedName = stat.charAt(0).toUpperCase() + stat.substring(1);
-			GAME.interface.statContainers[stat] = GAME.interface.initMeter(capitalizedName);
+			GAME.interface.statContainers[stat] = GAME.interface.addMeter(new GAME.interface.classes.statBar(capitalizedName));
 		}
 	}
 };
-
-
-////////////////////////////////////////////////////////////////////
-// Classes go here
-// So far only the statbars
-
-/**
- * Creates a meter bar and accompanying text. Inits the value to the max by default.
- *
- * @param name: The name displayed to the top left of the bar. Printed exactly as is.
- * @param max: Maximum value for the meter. Must be bigger than zero.
- *
- */
-GAME.interface.classes.statBar = function(name, max){
-	if (name == undefined){ alert('ERROR: Called the stat bar constructor without a name'); }
-	if (max < 1){ alert('ERROR: called the stat bar constructor with max set to less than one!'); }
-	if (max == undefined){ max = 100; }
-
-	// This is required for things with a maximum that isn't 100.
-	this.percentile = max / 100;
-
-	this.container = document.createElement('div');
-	this.topBar = document.createElement('p');
-	this.meterBar = document.createElement('meter');
-
-	this.container.className = 'statContainer';
-
-	///////////////////////////////////////
-	// Setting some sane values          //
-	this.topBar.innerHTML = name;        //
-	this.topBar.style.width = '100%';    //
-	this.topBar.dataset.value = 100+'%'; //
-	                                     //
-	this.meterBar.max = max;             //
-	this.meterBar.value = max;           //
-	///////////////////////////////////////
-
-	this.container.appendChild(this.topBar);
-	this.container.appendChild(this.meterBar);
-};
-GAME.interface.classes.statBar.prototype.updateValue = function(value){
-	var percentage = value / this.percentile;
-
-	this.topBar.style.width = (percentage > 50 ? percentage : 50)+'%'; // Don't let the value get TOO close to the text2
-	this.topBar.dataset.value = percentage + '%';
-
-	this.meter.value = sprintf('%f.1%%', percentage);
-};
-
-GAME.interface.classes.statBar.prototype.updateMax = function(newMax){
-	this.percentile = max / 100;
-	this.meterBar.max = newMax;
-	this.updateValue(this.meter.value); // Realign the top text.
-};
-
-////////////////////////////////////////////
